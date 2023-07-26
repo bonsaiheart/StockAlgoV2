7c81cb6b0126c5f19ed9217dd0b50988f147e666
bonsaiheart <nathanielwilliam117@gmail.com>
2023-07-26 14:04:53 -0400

    Changed main.py to continuosly run ib insetead of needing to run every min. Changed get_options_data to use less renaming, more efficiency

--- StockAlgoV2.bat
+++ StockAlgoV2.bat
@@ -1,1 +1,1 @@
-"C:\Users\natha\venv\Scripts\python.exe" "C:\Users\natha\PycharmProjects\StockAlgoV2\main.py" 
+"C:\Users\natha\StockAlgoV2\Scripts\python.exe" "C:\Users\natha\PycharmProjects\StockAlgoV2\main.py" 
